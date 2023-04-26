1a562fae6b2fb17f38ebc4577d78569de8dc405c
mr.gorros <felipe.rojas.s@alumnos.upla.cl>
2023-04-26 13:41:48 -0600

    clase 26/04/2023 periodo  1

---
+++ 1-operadores aritmeticos.py
@@ -0,0 +1,20 @@
+print("suma +") 
+print(3+2)
+print("resta -")
+print(3-2)
+print("multiplicacion *")
+print(3*2)
+print("divicicion")
+print(6/2)
+print("modulo")
+print(7 % 9)
+print("exponente")
+print(2**3)
+
+print("orden de predencia para operacionas aritmeticas")
+print("parentesis,potencia,raices,multiplicacion,divicion,suma y la resta \n")
+print(5+5*2)
+
+#tarea como hacer raiz cuadrada 
+# 25**(1/2)
+#averiguar que es la divicion entera
---
+++ variable.py
@@ -0,0 +1,17 @@
+numero = 3
+print(numero)
+
+numero2=5
+numero3=3
+
+print(numero2+numero3)
+
+resultado=numero2+numero3
+print(resultado)
+
+#reglas de nombre de variables
+#1.-no pueden empezar con un numero
+#2.-variables siempre en minusculas (recomendacion) 
+#3.-seprarar variables con un guin bajo (_)
+#4.-las variables no pueden tener un espacio
+#5.-NO pueden tener caracteres espaciales (con excepciones)
